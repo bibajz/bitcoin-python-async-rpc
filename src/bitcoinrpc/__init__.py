38b26169e4b30fdb66d1eb97b8c37839518288db
bibajz <libas@acco.cz>
2021-04-14 02:15:02 +0200

    Replace __all__ list with a tuple

--- src/bitcoinrpc/__init__.py
+++ src/bitcoinrpc/__init__.py
@@ -2,9 +2,9 @@ from .__version__ import __version__
 from ._exceptions import ImproperlyConfigured, RPCError
 from .bitcoin_rpc import BitcoinRPC
 
-__all__ = [
+__all__ = (
     "__version__",
     "BitcoinRPC",
     "ImproperlyConfigured",
     "RPCError",
-]
+)
