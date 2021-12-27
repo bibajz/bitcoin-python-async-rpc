e45fe2d69380c4c57789a35d1e0c9d87fb48b179
bibajz <libas@acco.cz>
2021-12-27 22:46:41 +0100

    Rework BitcoinRPC initialization
    Previously, `BitcoinRPC` accepted 2 arguments `host` and `port`,
    creating the BTC node url itself during the initialization in
    `_set_url` method.
    
    This way turned out to be inadequate, as was shown in an example
    when a library user wanted to connect to BTC node via https.
    
    Introduced changed in this commit delegate the url creation to the
    caller, via dropping the 2 arguments `host` and `port` in favour
    of `url`.

--- tests/test_client_config.py
+++ tests/test_client_config.py
@@ -11,7 +11,10 @@ def test_auth_provided_in_client_options_raises() -> None:
     """Authentication is meant to be passed directly to `BitcoinRPC`."""
     with pytest.raises(ImproperlyConfigured):
         BitcoinRPC(
-            "localhost", 8332, "rpc_user", "rpc_passwd", auth=("Not", "like this.")
+            "http://localhost:8332",
+            "rpc_user",
+            "rpc_passwd",
+            auth=("Not", "like this."),
         )
 
 
@@ -19,11 +22,13 @@ def test_unknown_httpx_option_provided_raises() -> None:
     """`httpx.AsyncClient` is strict about its init kwargs."""
     with pytest.raises(TypeError):
         BitcoinRPC(
-            "localhost", 8332, "rpc_user", "rpc_passwd", unknown_httpx_kwarg="foo"
+            "http://localhost:8332", "rpc_user", "rpc_passwd", unknown_httpx_kwarg="foo"
         )
 
 
 def test_incorrect_httpx_option_provided_raises() -> None:
     """Existing option, but incorrectly used, results in `httpx.AsyncClient` error."""
     with pytest.raises(AttributeError):
-        BitcoinRPC("localhost", 8332, "rpc_user", "rpc_passwd", limits="Not like this.")
+        BitcoinRPC(
+            "http://localhost:8332", "rpc_user", "rpc_passwd", limits="Not like this."
+        )
