5f3320b7c5d60ecdc8a18c3b8c01a3d8fe10ead7
bibajz <libas@acco.cz>
2023-05-29 23:42:09 +0200

    Instantiate BitcoinRPC directly with httpx.AsyncClient

--- src/bitcoinrpc/__init__.py
+++ src/bitcoinrpc/__init__.py
@@ -1,10 +1,9 @@
-from .__version__ import __version__
-from ._exceptions import ImproperlyConfigured, RPCError
-from .bitcoin_rpc import BitcoinRPC
+from bitcoinrpc.__version__ import __version__
+from bitcoinrpc._exceptions import RPCError
+from bitcoinrpc.bitcoin_rpc import BitcoinRPC
 
 __all__ = (
     "__version__",
     "BitcoinRPC",
-    "ImproperlyConfigured",
     "RPCError",
 )
